8c487a4cc33277fb7e3651c0f6f4006865321d7a
Itcor <itcorsmail@gmail.com>
2025-07-04 01:00:29 +0300

    lesson 3-2 is ready

---
+++ clessons-3-2-math-priority/lesson-3-2-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    double p = (a+b+c)/2;
+    
+    double result = sqrt(p*(p-a)*(p-b)*(p-c));
+    
+    printf("%.2lf", result);
+    
+    return 0;
+}
