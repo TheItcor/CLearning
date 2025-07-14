5473219f378f6f6f372cb524c9a5d1785a1f3789
Itcor <itcorsmail@gmail.com>
2025-07-14 16:16:25 +0300

    task 3

--- clessons-4-0-exam/lesson-4-0-3.c
+++ clessons-4-0-exam/lesson-4-0-3.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-  // put your code here
-  return 0;
+int main() 
+{
+    int k;
+    scanf("%d", &k);
+    
+    int minutes = k/60;
+    int hours = minutes/60;
+    printf("%d %d", hours, minutes-hours*60);
+    return 0;
 }
